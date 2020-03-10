241f8deff920833dd74c711dc836200439be10be
Admir Sezairi <sezairiadmir@gmail.com>
2020-03-10 07:53:36 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -2,8 +2,6 @@ require_relative './spec_helper'
 
 describe "say_hello" do
 
-  end
-
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
